3694c7a9403acae440d0ca61c834bae9d154a9f9
YlaxusB <souzagiovane2019@gmail.com>
2024-04-15 02:26:01 -0300

    Ludum Dare 55 Day 3

--- Assets/Scripts/CharacterController.cs
+++ Assets/Scripts/CharacterController.cs
@@ -9,9 +9,11 @@ using Plaza;
 using static Plaza.InternalCalls;
 using static Plaza.Input;
 using static Plaza.Physics;
+using System.Runtime.Remoting.Services;
 
 public class CharacterController : Entity
 {
+    public static int swordsCount = 8;
     public float gravity = -9.81f;
     public float totalGravity = 0.0f;
     public float sensitivity = 0.3f;
@@ -29,24 +31,43 @@ public class CharacterController : Entity
     public Vector3 startPosition = new Vector3(0.0f, 9.792f, 475.89f);
     public UInt64 terrainUuid = 0;
     public UInt64 previewUuid = 0;
-    public float heightSpawn = 50.0f;
+    public UInt64 towerUuid = 0;
+    public float heightSpawn = 20.0f;
     public DateTime lastJumpTime;
     public DateTime lastSummonTime;
+    public DateTime lastBallSummonTime;
     public DateTime startTime;
-    public int maxSeconds = 60;
+    public TimeSpan bestTime;
+    public int winCount = 0;
+    public int maxSeconds = 180;
 
     public UInt64 errorMaterialUuid = 1654307431041515234;
     public UInt64 previewMaterialUuid = 2522693449534569271;
+    public UInt64 entityToInstantiateUuid = 0;
+    public UInt64 ballToInstantiateUuid = 0;
+    public UInt64 redBallPreviewUuid = 0;
 
+    public Vector3 platformPosition = new Vector3(0.0f, 5.829f, 476.451f);
+    public Vector3 endPosition = new Vector3(-120, -51, -665);
+
+    public Vector2 swordsCountTextPos = new Vector2(0.0f, 50.0f);
+    public Vector2 bestTimeTextPos = new Vector2(0.0f, 100.0f);
+    public Vector2 sensitivityTextPos = new Vector2(0.0f, 500.0f);
 
     public Vector3 LerpVector(Vector3 start, Vector3 end, float time)
     {
         return start + (end - start) * time;
     }
 
-    public float lerpSpeed = 0.01f;
+    public float lerpSpeed = 0.07f;
     public float lerpTime = 1.0f;
     bool lastFrameRightButtonPressed = false;
+
+    public bool canRespawn = false;
+    public bool platformIsOnStart = false;
+    public bool showMenu = true;
+
+    public bool airJumpAvailable = true;
     public void OnStart()
     {
         gravity = -9.81f;
@@ -59,32 +80,58 @@ public class CharacterController : Entity
         groundDrag = 5.0f;
         jumpForce = 1300.0f;
         airMultiplier = 1.3f;
-        heightSpawn = 60.0f;
-        maxSeconds = 60;
+        heightSpawn = 20.0f;
+        maxSeconds = 180;
 
+        lerpSpeed = 0.07f;
 
         Cursor.Hide();
         terrainUuid = FindEntityByName("Terrain").Uuid;
         previewUuid = FindEntityByName("FlyingObjectPreviewHolder").Uuid;
+        towerUuid = FindEntityByName("tower").Uuid;
+        entityToInstantiateUuid = FindEntityByName("SwordSample").Uuid;
+        ballToInstantiateUuid = FindEntityByName("RedBall").Uuid;
+        redBallPreviewUuid = FindEntityByName("RedBallPreview").Uuid;
 
         this.AddComponent<TextRenderer>();
+        FindEntityByName("SwordsCountText").AddComponent<TextRenderer>();
+        FindEntityByName("SensitivityText").AddComponent<TextRenderer>();
         startTime = DateTime.Now;
+
+        canRespawn = true;
     }
-    public bool canRespawn = false;
     public void OnUpdate()
     {
-
+        // if (showMenu)
+        // {
+        //     if (Input.IsKeyDown(KeyCode.Uknown))
+        //     {
+        //         ReadBook();
+        //         showMenu = false;
+        //     }
+        // }
         this.GetComponent<TextRenderer>().SetText((maxSeconds - (DateTime.Now - startTime).TotalSeconds).ToString(), 0, 0, 3);
-        if ((startTime - DateTime.Now).TotalSeconds < -maxSeconds)
+        FindEntityByName("SwordsCountText").GetComponent<TextRenderer>().SetText("Swords: " + swordsCount.ToString(), swordsCountTextPos.X, swordsCountTextPos.Y, 3);
+        if (winCount > 0)
+            FindEntityByName("BestTimeText").GetComponent<TextRenderer>().SetText("Best Time: " + bestTime.ToString(), bestTimeTextPos.X, bestTimeTextPos.Y, 3);
+
+        if ((startTime - DateTime.Now).TotalSeconds < -maxSeconds || CharacterController.swordsCount <= 0 || this.GetComponent<Transform>().Translation.Y < -100.0f)
             canRespawn = true;
 
         if (canRespawn)
         {
+            PlayDeathSound();
+
+            FindEntityByName("Platform").GetComponent<Transform>().Translation = platformPosition;
             this.GetComponent<RigidBody>().ApplyForce(new Vector3(0.0f));
+            this.GetComponent<RigidBody>().velocity = new Vector3(0.0f);
             this.GetComponent<Transform>().Translation = startPosition;
+            TravellerObjectsManager.Reset(startPosition);
 
             startTime = DateTime.Now;
             canRespawn = false;
+            CharacterController.swordsCount = 8;
+            platformIsOnStart = true;
         }
         else
         {
@@ -94,6 +141,54 @@ public class CharacterController : Entity
 
             HandleBook();
         }
+        // Check if player reached the end
+        //if (Vector3.Distance(this.GetComponent<Transform>().Translation, endPosition) < 250.0f)
+        //{
+        //    Win();
+        //}
+
+        // Move the platform away if player moved
+        if (platformIsOnStart && Vector3.Distance(this.GetComponent<Transform>().Translation, platformPosition) > 10.0f)
+        {
+            FindEntityByName("Platform").GetComponent<Transform>().Translation = new Vector3(0.0f, -1000.0f, 0.0f);
+            platformIsOnStart = false;
+        }
+    }
+
+    public static void PlayDeathSound()
+    {
+
+    }
+
+    public static void PlaySwordSummonSound()
+    {
+
+    }
+
+    public static void PlaySwordPointSound()
+    {
+       
+    }
+
+    public static void PlayFallSound()
+    {
+
+    }
+
+    public static void PlayWinSound()
+    {
+
+    }
+
+    public void Win()
+    {
+        PlayWinSound();
+        bestTime = DateTime.Now - startTime;
+        winCount++;
+
+        if (winCount == 1)
+            FindEntityByName("BestTimeText").AddComponent<TextRenderer>();
+        canRespawn = true;
     }
 
     public void HandleBook()
@@ -101,15 +196,16 @@ public class CharacterController : Entity
         bool isSummonCooldownOver = (lastSummonTime - DateTime.Now).TotalSeconds < -1.0f;
 
         RaycastHit hit;
-        Physics_Raycast(this.GetComponent<Transform>().Translation + FindEntityByName("CameraEntity").GetComponent<Transform>().Translation, FindEntityByName("CameraEntity").GetComponent<Transform>().LeftVector * new Vector3(-1.0f), 10000.0f, out hit, previewUuid);
+        Physics_RaycastSpecific(this.GetComponent<Transform>().Translation + FindEntityByName("CameraEntity").GetComponent<Transform>().Translation, FindEntityByName("CameraEntity").GetComponent<Transform>().LeftVector * new Vector3(-1.0f), 10000.0f, out hit, terrainUuid);
         /* Spawn swords */
         if (isSummonCooldownOver && Input.IsMouseDown(0))
         {
-            Console.WriteLine(hit.hitUuid);
-            if (hit.hitUuid == terrainUuid)
+            if (hit.hitUuid == terrainUuid && CharacterController.swordsCount > 0)
             {
-                Entity newTruck = Instantiate(FindEntityByName("SwordSample"));
-                newTruck.GetComponent<Transform>().Translation = new Entity(hit.hitUuid).GetComponent<Transform>().Translation + hit.point + new Vector3(0.0f, heightSpawn, 0.0f);
+                PlaySwordSummonSound();
+
+                Entity newTruck = Instantiate(new Entity(entityToInstantiateUuid));
+                newTruck.GetComponent<Transform>().Translation = FindEntityByName("FlyingObjectPreviewHolder").GetComponent<Transform>().Translation;//new Entity(hit.hitUuid).GetComponent<Transform>().Translation + hit.point + new Vector3(0.0f, heightSpawn, 0.0f);
 
                 InternalCalls.GetWorldRotationQuaternionCall(FindEntityByName("FlyingObjectPreviewSword").Uuid, out Vector4 rotation);
                 Quaternion rotationQuaternion = new Quaternion(rotation.X, rotation.Y, rotation.Z, rotation.W);
@@ -117,13 +213,23 @@ public class CharacterController : Entity
                 newTruck.GetComponent<Transform>().Rotation = rotationQuaternion;
 
                 TravellerObjectsManager.flyingObjects.Add(newTruck.Uuid);
+                CharacterController.swordsCount--;
                 lastSummonTime = DateTime.Now;
             }
         }
         else if (hit.hitUuid == terrainUuid)
         {
-            FindEntityByName("FlyingObjectPreviewHolder").GetComponent<Transform>().Translation = new Entity(hit.hitUuid).GetComponent<Transform>().Translation + hit.point + new Vector3(0.0f, heightSpawn, 0.0f);
-            FindEntityByName("FlyingObjectPreviewHolder").GetComponent<Transform>().Rotation = this.GetComponent<Transform>().Rotation;
+            FindEntityByName("FlyingObjectPreviewHolder").GetComponent<Transform>().Translation = hit.point + new Vector3(0.0f, heightSpawn, 0.0f);
+
+            /* Calculate yaw */
+            float yaw = QuaternionToEulerAngles(this.GetComponent<Transform>().Rotation).Y * Mathf.Rad2Deg;
+            if (yaw > 0.0f && yaw < 180.0f) // Only rotate if yaw is not heading to the end
+            {
+                yaw = yaw * Mathf.Deg2Rad;
+                yaw = Clamp(yaw, 0.0f, 180.0f);
+                FindEntityByName("FlyingObjectPreviewHolder").GetComponent<Transform>().Rotation = Vector3ToQuaternion(new Vector3(0.0f, yaw, 0.0f));//this.GetComponent<Transform>().Rotation;
+            }
+
         }
 
         FindEntityByName("FlyingObjectPreviewSword").GetComponent<MeshRenderer>().SetMaterial(isSummonCooldownOver ? previewMaterialUuid : errorMaterialUuid);
@@ -131,11 +237,34 @@ public class CharacterController : Entity
         ReadBook();
 
         ThrowPower();
+
+        HandleSensitivityControl();
     }
 
     public void ThrowPower()
     {
+        bool isBallSummonCooldownOver = (lastBallSummonTime - DateTime.Now).TotalSeconds < -0.7f;
+        if (isBallSummonCooldownOver && Input.IsMouseDown(1))
+        {
+            Entity newBall = Instantiate(new Entity(ballToInstantiateUuid));
+            newBall.GetComponent<Transform>().Translation = FindEntityByName("CameraEntity").GetComponent<Transform>().Translation + FindEntityByName("Player").GetComponent<Transform>().Translation;//new Entity(hit.hitUuid).GetComponent<Transform>().Translation + hit.point + new Vector3(0.0f, heightSpawn, 0.0f);
 
+            InternalCalls.GetWorldRotationQuaternionCall(redBallPreviewUuid, out Vector4 rotation);
+            Quaternion rotationQuaternion = new Quaternion(rotation.X, rotation.Y, rotation.Z, rotation.W);
+            rotationQuaternion.Normalize();
+            newBall.GetComponent<Transform>().Rotation = rotationQuaternion;
+            TravellerObjectsManager.flyingBalls.Add(newBall.Uuid);
+            lastBallSummonTime = DateTime.Now;
+        }
+        else
+        {
+            Entity redBallHolder = FindEntityByName("RedBallHolder");
+            redBallHolder.GetComponent<Transform>().Translation = FindEntityByName("CameraEntity").GetComponent<Transform>().Translation + FindEntityByName("Player").GetComponent<Transform>().Translation;//new Entity(hit.hitUuid).GetComponent<Transform>().Translation + hit.point + new Vector3(0.0f, heightSpawn, 0.0f);
+            redBallHolder.GetComponent<Transform>().Rotation = this.GetComponent<Transform>().Rotation;
+
+            Entity redBallPreview = new Entity(redBallPreviewUuid);
+            redBallPreview.GetComponent<Transform>().Rotation = FindEntityByName("CameraEntity").GetComponent<Transform>().Rotation;
+        }
     }
 
     public void ReadBook()
@@ -148,24 +277,26 @@ public class CharacterController : Entity
         lerpTime = Math.Min(lerpTime, 1.0f);
         lerpTime = Math.Max(lerpTime, 0.0f);
         Transform pageTransform = FindEntityByName("page").GetComponent<Transform>();
-        if (Input.IsMouseDown(1))
-        {
-            pageTransform.Translation = LerpVector(bookReadPosition, pageTransform.Translation, lerpTime);
-            pageTransform.Scale = LerpVector(bookReadScale, pageTransform.Scale, lerpTime);
-            lastFrameRightButtonPressed = true;
-        }
-        else
-        {
-            pageTransform.Translation = LerpVector(bookIdlePosition, pageTransform.Translation, lerpTime);
-            pageTransform.Scale = LerpVector(bookIdleScale, pageTransform.Scale, lerpTime);
-            lastFrameRightButtonPressed = false;
-        }
+        //if (showMenu)
+        //{
+        //    pageTransform.Translation = LerpVector(bookReadPosition, pageTransform.Translation, lerpTime);
+        //    pageTransform.Scale = LerpVector(bookReadScale, pageTransform.Scale, lerpTime);
+        //    lastFrameRightButtonPressed = true;
+        //}
+        //else
+        //{
+        pageTransform.Translation = LerpVector(bookIdlePosition, pageTransform.Translation, lerpTime);
+        pageTransform.Scale = LerpVector(bookIdleScale, pageTransform.Scale, lerpTime);
+        lastFrameRightButtonPressed = false;
+        //}
     }
 
     public void OnCollide(UInt64 collidedUuid, Vector3 hitPosition)
     {
         if (collidedUuid == terrainUuid)
             canRespawn = true;
+        if (collidedUuid == towerUuid)
+            Win();
         //this.GetComponent<Transform>().Translation = startPosition;
     }
 
@@ -212,6 +343,7 @@ public class CharacterController : Entity
 
         force = this.GetComponent<Transform>().LeftVector * -vertical + this.GetComponent<Transform>().ForwardVector * horizontal;
         force += new Vector3(0.0f, totalGravity, 0.0f);
+        force *= Time.deltaTime * 100.0f;
 
         if (OnSlope(hit))
         {
@@ -225,17 +357,12 @@ public class CharacterController : Entity
                 this.GetComponent<RigidBody>().AddForce(force * airMultiplier, ForceMode.FORCE);
         }
 
-        bool isCoolDownOver = (lastJumpTime - DateTime.Now).TotalSeconds < -1.0f;
         if (hittingGround)
         {
             totalGravity = gravity;
             this.GetComponent<RigidBody>().drag = groundDrag;
-            if (Input.IsKeyDown(KeyCode.Space) && isCoolDownOver)
-            {
-                //this.GetComponent<RigidBody>().ApplyForce(new Vector3(0.0f, jumpHeight, 0.0f));
-                Jump();
-                lastJumpTime = DateTime.Now;
-            }
+
+            airJumpAvailable = true;
         }
         else
         {
@@ -243,13 +370,32 @@ public class CharacterController : Entity
             totalGravity += gravity;
         }
 
+        HandleJump(hittingGround);
+
+    }
+
+    public void HandleJump(bool hittingGround)
+    {
+        bool isJumpCoolDownOver = (lastJumpTime - DateTime.Now).TotalSeconds < -1.0f;
+
+        if (Input.IsKeyDown(KeyCode.Space) && isJumpCoolDownOver && (hittingGround || airJumpAvailable))
+        {
+            //this.GetComponent<RigidBody>().ApplyForce(new Vector3(0.0f, jumpHeight, 0.0f));
+            Jump();
+            lastJumpTime = DateTime.Now;
+            if (!hittingGround)
+                airJumpAvailable = false;
+        }
     }
 
     public void Jump()
     {
+        totalGravity = 0.0f;
         RigidBody rigidBody = this.GetComponent<RigidBody>();
         rigidBody.velocity = new Vector3(rigidBody.velocity.X, 0.0f, rigidBody.velocity.Z);
 
+        rigidBody.velocity = new Vector3(rigidBody.velocity.X, 0.0f, rigidBody.velocity.Z);
+        rigidBody.ApplyForce(new Vector3(0.0f, 0.0f, 0.0f));
         rigidBody.AddForce(new Vector3(0.0f, 1.0f, 0.0f) * jumpForce, ForceMode.IMPULSE);
 
     }
@@ -292,5 +438,99 @@ public class CharacterController : Entity
         this.GetComponent<Transform>().Rotation *= quat;
 
         FindEntityByName("CameraEntity").GetComponent<Transform>().Rotation *= new Quaternion(new Vector3(0.0f, 0.0f, -Input.MouseDeltaY() * sensitivity * Time.deltaTime));
+    }
+
+    public static float Clamp(float angle, float minAngle, float maxAngle)
+    {
+        return Math.Min(Math.Max(angle, minAngle), maxAngle);
+    }
+
+    public static Vector3 QuaternionToEulerAngles(Quaternion q)
+    {
+        // Extract sin/cos of yaw, pitch, roll angles
+        float sinYaw = 2.0f * (q.w * q.y + q.x * q.z);
+        float cosYaw = 1.0f - 2.0f * (q.y * q.y + q.z * q.z);
+        float sinPitch = 2.0f * (q.w * q.x - q.y * q.z);
+        float cosPitch = 1.0f - 2.0f * (q.x * q.x + q.z * q.z);
+
+        // Calculate yaw (heading) angle
+        float yaw = (float)Math.Atan2(sinYaw, cosYaw);
+
+        // Calculate pitch angle
+        float pitch;
+        if (Math.Abs(sinPitch) >= 1)
+            pitch = (float)Math.PI / 2 * Math.Sign(sinPitch); // Use +/-90 degrees if singularity
+        else
+            pitch = (float)Math.Asin(sinPitch);
+
+        // No need to calculate roll for heading direction
+
+        // Return Euler angles as Vector3
+        return new Vector3(pitch, yaw, 0);
+    }
+
+    public static float CalculateYawFromQuaternion(Quaternion quaternion)
+    {
+        // Ensure the quaternion is normalized
+        quaternion.Normalize();
+
+        // Extract quaternion components
+        float q_w = quaternion.w;
+        float q_x = quaternion.x;
+        float q_y = quaternion.y;
+        float q_z = quaternion.z;
+
+        // Calculate yaw angle
+        float yaw = (float)Math.Atan2(2 * (q_w * q_z + q_x * q_y), 1 - 2 * (q_y * q_y + q_z * q_z));
+
+        // Convert radians to degrees
+        yaw = yaw * Mathf.Rad2Deg;
+
+        // Ensure the yaw angle is within [0, 360) range
+        if (yaw < 0)
+        {
+            yaw += 360;
+        }
+
+        return yaw;
+    }
+
+    public static Quaternion Vector3ToQuaternion(Vector3 axisRotations)
+    {
+        // Convert Euler angles to radians
+        float yaw = axisRotations.Y * 0.5f;
+        float pitch = axisRotations.X * 0.5f;
+        float roll = axisRotations.Z * 0.5f;
+
+        // Calculate sin/cos of half angles
+        float sinYaw = (float)Math.Sin(yaw);
+        float cosYaw = (float)Math.Cos(yaw);
+        float sinPitch = (float)Math.Sin(pitch);
+        float cosPitch = (float)Math.Cos(pitch);
+        float sinRoll = (float)Math.Sin(roll);
+        float cosRoll = (float)Math.Cos(roll);
+
+        // Calculate quaternion components
+        float x = cosYaw * sinPitch * cosRoll + sinYaw * cosPitch * sinRoll;
+        float y = sinYaw * cosPitch * cosRoll - cosYaw * sinPitch * sinRoll;
+        float z = cosYaw * cosPitch * sinRoll - sinYaw * sinPitch * cosRoll;
+        float w = cosYaw * cosPitch * cosRoll + sinYaw * sinPitch * sinRoll;
+
+        // Return the quaternion
+        return new Quaternion(x, y, z, w);
+    }
+
+    public void HandleSensitivityControl()
+    {
+        if (Input.IsKeyDown(KeyCode.PageUp))
+        {
+            sensitivity = Clamp(sensitivity + (sensitivity * 0.10f * Time.deltaTime), 0.01f, 2.0f);
+            FindEntityByName("SensitivityText").GetComponent<TextRenderer>().SetText("Sensitivity: " + ((short)sensitivity).ToString(), sensitivityTextPos.X, sensitivityTextPos.Y, 1.5f);
+        }
+        else if (Input.IsKeyDown(KeyCode.PageDown))
+        {
+            sensitivity = Clamp(sensitivity - (sensitivity * 0.10f * Time.deltaTime), 0.01f, 2.0f); ;
+            FindEntityByName("SensitivityText").GetComponent<TextRenderer>().SetText("Sensitivity: " + ((short)sensitivity).ToString(), sensitivityTextPos.X, sensitivityTextPos.Y, 1.5f); ;
+        }
     }
 }
--- Assets/Scripts/TrucksManager.cs
+++ Assets/Scripts/TrucksManager.cs
@@ -8,14 +8,85 @@ using System.Threading.Tasks;
 using Plaza;
 using static Plaza.InternalCalls;
 using static Plaza.Input;
+using static Plaza.Physics;
 
 public class TravellerObjectsManager : Entity
 {
     public static float flyingObjectsSpeed = 48.0f;
+    public static float cactusSpeed = 100.0f;
+    public static float flyingBallSpeed = 336.0f;
     public static List<UInt64> flyingObjects = new List<UInt64>();
+    public static List<UInt64> flyingBalls = new List<UInt64>();
+    public static List<Vector3> staticSwordsPosition = new List<Vector3>();
+    public static UInt64 terrainUuid = 0;
+    public static List<UInt64> staticSwordsEntityUuid = new List<UInt64>();
+
+    public static List<UInt64> flyingCactusUuids = new List<UInt64>();
+    public static List<Vector3> flyingCactusPositions = new List<Vector3>();
+
+    public static Random random = new Random();
     public void OnStart()
     {
+        terrainUuid = FindEntityByName("Terrain").Uuid;
 
+        for (int i = 0; i < 8; ++i)
+        {
+            Entity newStaticSword = Instantiate(FindEntityByName("StaticSword"));
+            staticSwordsEntityUuid.Add(newStaticSword.Uuid);
+
+            Vector3 randomPosition = GetStaticSwordPosition(GetCorrectedCenter(FindEntityByName("Player").GetComponent<Transform>().Translation));
+            newStaticSword.GetComponent<Transform>().Translation = randomPosition;
+            staticSwordsPosition.Add(randomPosition);
+        }
+
+        for (int i = 0; i < 4; ++i)
+        {
+            Entity instantiatedCactus = Instantiate(FindEntityByName("Cactus1"));
+            instantiatedCactus.Name = "CactusA" + i;
+            flyingCactusUuids.Add(instantiatedCactus.Uuid);
+            flyingCactusPositions.Add(GenerateRandomCactusPosition(FindEntityByName("Player").GetComponent<Transform>().Translation));
+        }
+    }
+
+    public static Vector3 GetCorrectedCenter(Vector3 vector)
+    {
+        return new Vector3(0.0f, 0.0f, 0.0f);
+        Vector3 correctedCenter = vector;
+        correctedCenter.Z = Math.Min(correctedCenter.Z, -250.0f);
+        correctedCenter.Z = Math.Max(correctedCenter.Z, 250.0f);
+        return correctedCenter;
+    }
+
+    public static Vector3 GetStaticSwordPosition(Vector3 center)
+    {
+        Random random = new Random();
+        Vector3 randomPosition = new Vector3((float)random.NextDouble() * 500 - 250, 1000.0f, (float)random.NextDouble() * 800 - 400);
+        randomPosition += center;
+        RaycastHit hit;
+        Physics_RaycastSpecific(randomPosition, new Vector3(0.0f, -1.0f, 0.0f), 2000.0f, out hit, terrainUuid);
+        randomPosition.Y = hit.point.Y + 75.0f;
+        return randomPosition;
+    }
+
+    public static void Reset(Vector3 playerPosition)
+    {
+        /* Regenerate static swords */
+        for (int j = 0; j < staticSwordsPosition.Count; ++j)
+        {
+            CharacterController.swordsCount++;
+
+            Vector3 randomPosition = GetStaticSwordPosition(GetCorrectedCenter(playerPosition));
+            new Entity(staticSwordsEntityUuid[j]).GetComponent<Transform>().Translation = randomPosition;
+            staticSwordsPosition[j] = randomPosition;
+
+            continue;
+        }
+
+        /* Regenerate Cactus */
+        for (int i = 0; i < flyingCactusPositions.Count; ++i)
+        {
+            RegenerateCactus(i, playerPosition);
+        }
     }
 
     public void OnUpdate()
@@ -24,15 +95,86 @@ public class TravellerObjectsManager : Entity
         flyingObjectsSpeed = 48.0f;
         for (int i = 0; i < flyingObjects.Count; ++i)
         {
-            if (Vector3.Distance(new Entity(flyingObjects[i]).GetComponent<Transform>().Translation, playerPosition) > 500.0f)
+            if (Vector3.Distance(new Entity(flyingObjects[i]).GetComponent<Transform>().Translation, playerPosition) > 1500.0f)
             {
                 flyingObjects.RemoveAt(i);
+                //InternalCalls.EntityDelete(flyingObjects[i]);
                 //new Entity(flyingObjects[i]).Delete();
                 continue;
             }
             new Entity(flyingObjects[i]).GetComponent<Transform>().MoveTowards(new Vector3(0.0f, flyingObjectsSpeed * Time.deltaTime, 0.0f));
             //trucks[i].GetComponent<RigidBody>().AddForce(new Vector3(100000.0f, 0.0f, 0.0f) * Time.deltaTime);
         }
+
+        for (int i = 0; i < flyingBalls.Count; ++i)
+        {
+            Vector3 ballPosition = new Entity(flyingBalls[i]).GetComponent<Transform>().Translation;
+            if (Vector3.Distance(ballPosition, playerPosition) > 1500.0f)
+            {
+                flyingBalls.RemoveAt(i);
+                continue;
+            }
+            for (int j = 0; j < staticSwordsPosition.Count; ++j)
+            {
+                if (Vector3.Distance(ballPosition, staticSwordsPosition[j]) < 30.0f)
+                {
+                    HitStaticSword(i, j);
+                    continue;
+                }
+            }
+
+            new Entity(flyingBalls[i]).GetComponent<Transform>().MoveTowards(new Vector3(0.0f, 0.0f, flyingBallSpeed * Time.deltaTime));
+        }
+
+        UpdateCactus(playerPosition);
+    }
+
+    public void HitStaticSword(int i, int j)
+    {
+        CharacterController.PlaySwordPointSound();
+        CharacterController.swordsCount += 3;
+
+        Vector3 randomPosition = GetStaticSwordPosition(GetCorrectedCenter(FindEntityByName("Player").GetComponent<Transform>().Translation));
+        new Entity(staticSwordsEntityUuid[j]).GetComponent<Transform>().Translation = randomPosition;
+        staticSwordsPosition[j] = randomPosition;
+
+        InternalCalls.EntityDelete(flyingBalls[i]);
+        flyingBalls.RemoveAt(i);
+    }
+
+    public void UpdateCactus(Vector3 playerPosition)
+    {
+        for (int i = 0; i < flyingCactusUuids.Count; ++i)
+        {
+            Entity cactus = new Entity(flyingCactusUuids[i]);
+            RaycastHit hit;
+            Physics_RaycastSpecific(flyingCactusPositions[i] + new Vector3(0.0f, 0.0f, cactusSpeed * Time.deltaTime), new Vector3(0.0f, -1.0f, 0.0f), 1000.0f, out hit, terrainUuid);
+            cactus.GetComponent<Transform>().Translation = hit.point + new Vector3(30.0f);
+            flyingCactusPositions[i] = hit.point + new Vector3(0.0f, 300.0f, 0.0f);
+
+            if (hit.hitUuid == 0 || playerPosition.Z - hit.point.Z < 10.0f) 
+            {
+                RegenerateCactus(i, FindEntityByName("Player").GetComponent<Transform>().Translation);
+            }
+        }
+    }
+
+    public static Vector3 GenerateRandomCactusVector()
+    {
+        return new Vector3((float)random.NextDouble() * 650.0f - 325.0f, 100.0f, -500.0f);
+    }
+
+    public static Vector3 GenerateRandomCactusPosition(Vector3 playerPosition)
+    {
+        Vector3 randomVector = GenerateRandomCactusVector();
+        if (Vector3.Distance(randomVector, playerPosition) < 10.0f)
+            return GenerateRandomCactusPosition(playerPosition);
+        return randomVector;
+    }
+
+    public static void RegenerateCactus(int index, Vector3 playerPosition)
+    {
+        flyingCactusPositions[index] = GenerateRandomCactusPosition(playerPosition);
     }
 
     public void OnRestart()
